efb854acbfbf07d8213e9ef87cb961fcfc5c7624
lucdo <lucas.dominguez@etu.univ-cotedazur.fr>
2021-03-24 16:47:13 +0100

    session 5 multiple endpoint

--- MathsOperations/Addition/Program.cs
+++ MathsOperations/Addition/Program.cs
@@ -1,21 +1,21 @@
-﻿using Addition.ServiceReference1;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System;
 namespace Addition
 {
     class Program
     {
         static void Main(string[] args)
         {
-            MathsOperationsClient m = new MathsOperationsClient();
-            Console.WriteLine(m.Add(1, 2));
-            Console.WriteLine(m.Multiply(2,3));
-            Console.WriteLine(m.Subtract(1, 10));
-            Console.WriteLine(m.Divide(8, 3));
+            //ServiceReference1.MathsOperationsClient m = new ServiceReference1.MathsOperationsClient();
+            ServiceReference2.MathsOperationsClient m1 = new ServiceReference2.MathsOperationsClient("SoapEnd1");
+            ServiceReference2.MathsOperationsClient m2 = new ServiceReference2.MathsOperationsClient("SoapEnd2");
+            Console.WriteLine(m1.Add(1, 2));
+            Console.WriteLine(m1.Multiply(2,3));
+            Console.WriteLine(m1.Subtract(1, 10));
+            Console.WriteLine(m1.Divide(8, 3));
+            Console.WriteLine(m2.Add(5, 5));
+            Console.WriteLine(m2.Multiply(5, 5));
+            Console.WriteLine(m2.Subtract(5, 5));
+            Console.WriteLine(m2.Divide(5, 5));
             Console.ReadLine();
         }
     }
